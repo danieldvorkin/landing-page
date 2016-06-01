8ea6dd8d0d497fc9cba71498eb864e8e70b25f40
Daniel Dvorkin <Dvorkin212@gmail.com>
2016-05-31 21:29:01 -0400

    did some work on the js side, still not full working though

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,23 +1,26 @@
+function retrieveMonths(data){
+	var months = []
+	$.each(data.numbers, function(i) {
+		months.push(data.numbers[i]['month']);
+	});
+	return months;
+}
+
+function getData(){
+	$.getJSON("http://" + window.location.host + "/js/chart.json", function(data){
+		console.log(data);
+		return data;
+  });
+}
+
+
 $(function(){
-	$('.btn-default').click(function(e){
-		console.log('clicked');
-		$.getJSON("chart.json", function(data){
-		  var items = [];
-		  $.each( data, function( key, val ) {
-		    items.push(key + ": " + val);
-		  });
-		 
-		  console.log(items);
-		});
-	})
-	
+	var data = retrieveMonths(getData());
+	console.log(data);
 	
 	// line chart data
 	var iniData = {
-		labels:[
-			"2000","2001","2002","2003","2004","2005","2006",
-			"2007","2008","2009","2011","2012","2013","2014",
-			"2015","2016","2017","2018","2019","2020"],
+		labels: data,
 		datasets:[{
 			fillColor:"rgba(172,194,132,0)",
 			strokeColor:"#C45662",
